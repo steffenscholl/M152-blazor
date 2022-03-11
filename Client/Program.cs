7cb72befe843fcfb75c0b148dbc53ea0413e15cb
steffenscholl <82201092+steffenscholl@users.noreply.github.com>
2022-03-11 13:14:38 +0100

    Revert "klappet"
    This reverts commit ad61cb312a975fa5e6e7c4b7a0878c97d49e075c.

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,10 +9,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
+
+
 builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
 builder.Services.AddScoped<TooltipService>();
-builder.Services.AddScoped<ContextMenuService>(); 
-//builder.Services.AddHttpClient<IHttpClientFactory>();
+builder.Services.AddScoped<ContextMenuService>();
 await builder.Build().RunAsync();
