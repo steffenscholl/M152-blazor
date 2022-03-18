28159be74cdb496dfc3a64b1f1598eee5c761c51
steffenscholl <82201092+steffenscholl@users.noreply.github.com>
2022-03-18 14:34:25 +0100

    no idea if it works

--- Client/Program.cs
+++ Client/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
 using Radzen;
+using Blazorise;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,7 +11,11 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
-
+builder.Services.AddBlazorise(options =>
+ {
+     options.Immediate = true;
+ })
+    .AddFontAwesomeIcons();
 
 builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<DialogService>();
