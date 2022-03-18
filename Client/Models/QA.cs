a9bc2319d89b476f5995b99e945a94c7ea674bd6
steffenscholl <82201092+steffenscholl@users.noreply.github.com>
2022-03-18 15:19:21 +0100

    WORKS DONT CHANGE

---
+++ Client/Models/QA.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp.Client.Models
+{
+    public class QA
+    {
+        public string Question { get; set; } = string.Empty;
+        public string Answer { get; set; } = string.Empty;   
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -2,7 +2,9 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
 using Radzen;
+using BytexDigital.Blazor.Components.CookieConsent;
 using Blazorise;
+using Blazorise.Bootstrap;
 using Blazorise.Icons.FontAwesome;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -11,15 +13,75 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddBlazorise(options =>
- {
-     options.Immediate = true;
- })
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrapProviders()
     .AddFontAwesomeIcons();
 
-builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<DialogService>();
 builder.Services.AddScoped<NotificationService>();
 builder.Services.AddScoped<TooltipService>();
 builder.Services.AddScoped<ContextMenuService>();
+
+builder.Services.AddCookieConsent(o =>
+{
+    o.Revision = 1;
+    o.ConsentModalPosition = ConsentModalPosition.BottomRight;
+    o.ConsentModalLayout = ConsentModalLayout.Cloud;
+    o.ConsentSecondaryActionOpensSettings = false;
+
+    o.Categories.Add(new CookieCategory
+    {
+        TitleText = new()
+        {
+            ["en"] = "Google Services",
+            ["de"] = "Google Dienste"
+        },
+        DescriptionText = new()
+        {
+            ["en"] = "Allows the integration and usage of Google services.",
+            ["de"] = "Erlaubt die Verwendung von Google Diensten."
+        },
+        Identifier = "google",
+        IsPreselected = true,
+
+        Services = new()
+        {
+            new CookieCategoryService
+            {
+                Identifier = "google-maps",
+                PolicyUrl = "https://policies.google.com/privacy",
+                TitleText = new()
+                {
+                    ["en"] = "Google Maps",
+                    ["de"] = "Google Maps"
+                },
+                ShowPolicyText = new()
+                {
+                    ["en"] = "Display policies",
+                    ["de"] = "Richtlinien anzeigen"
+                }
+            },
+            new CookieCategoryService
+            {
+                Identifier = "google-analytics",
+                PolicyUrl = "https://policies.google.com/privacy",
+                TitleText = new()
+                {
+                    ["en"] = "Google Analytics",
+                    ["de"] = "Google Analytics"
+                },
+                ShowPolicyText = new()
+                {
+                    ["en"] = "Display policies",
+                    ["de"] = "Richtlinien anzeigen"
+                }
+            }
+        }
+    });
+});
+
 await builder.Build().RunAsync();
